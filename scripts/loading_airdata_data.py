a043f93dbf264ce74e49090413804470cbd4a064
Ishan Nangia <ishannangia.123@gmail.com>
2024-04-26 16:13:20 +0530

    add preprocessing steps for airdata df

--- scripts/loading_airdata_data.py
+++ scripts/loading_airdata_data.py
@@ -12,4 +12,16 @@ dt_formats = ['%Y-%m-%d %H:%M:%S']
 
 adl = AirdataDataLoader(airdata_folders)
 adl.load_data()
-adl.preprocess_cols(float_cols, dt_cols, dt_formats)
+df = adl.preprocess_cols(float_cols, dt_cols, dt_formats)
+
+# convert dt column
+df['datetime(utc)'] = df['datetime(utc)'].dt.tz_localize("utc").dt.tz_convert('Asia/Kolkata')
+df.rename({"datetime(utc)": 'datetime(ist)'}, axis=1, inplace=True)
+
+# round of cols
+df['latitude'] = df['latitude'].round(6)
+df['longitude'] = df['longitude'].round(6)
+
+# selecting subset
+df = df[df['isPhoto'] == 1]
+df.reset_index(drop=True, inplace=True)
