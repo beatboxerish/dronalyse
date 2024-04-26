614639cf219384d641d6eb6e5fd732b12de0f20e
Ishan Nangia <ishannangia.123@gmail.com>
2024-04-26 15:14:24 +0530

    add load_data function to AirdataDataLoader

--- dronalyse/loaders/airdata_data_loader.py
+++ dronalyse/loaders/airdata_data_loader.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from dronalyse.utils.airdata_utils import get_airdata_files_from_folder
 
 
@@ -7,7 +8,20 @@ class AirdataDataLoader:
         """Initializes AirdataDataLoader class to load and play with airdata-based data"""
         self.airdata_folders = airdata_folders  # folders containing images
         self.all_airdata_files = []
+        self.all_airdata_df = None
 
         for airdata_folder in airdata_folders:
             list_of_files = get_airdata_files_from_folder(airdata_folder)
             self.all_airdata_files.extend(list_of_files)
+
+    # TODO: standardise how different classes store data. Should be the same type of object.
+    def load_data(self, airdata_keys=None):
+        self.all_airdata_df = None
+        # read all files one by one and get all data together
+        for file_name in self.all_airdata_files:
+            for airdata_file in self.all_airdata_files:
+                current_df = pd.read_csv(airdata_file)
+                self.all_airdata_df = pd.concat([self.all_airdata_df, current_df], axis=0)
+            self.all_airdata_df.reset_index(drop=True, inplace=True)
+            if airdata_keys:
+                self.all_airdata_df = self.all_airdata_df[airdata_keys]
--- scripts/loading_airdata_data.py
+++ scripts/loading_airdata_data.py
@@ -1,5 +1,9 @@
 from dronalyse.loaders.airdata_data_loader import AirdataDataLoader
 
 airdata_folders = ["/Users/ishannangia/Desktop/TfW/photogrammetry expt/airdata/"]
+keys_for_airdata = ["datetime(utc)", "latitude", "longitude",
+                   'height_above_takeoff(meters)', 'height_above_ground_at_drone_location(meters)',
+                   'height_sonar(meters)', 'isPhoto', 'isVideo']
 
 adl = AirdataDataLoader(airdata_folders)
+adl.load_data()
